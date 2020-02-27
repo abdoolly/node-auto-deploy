eb8eaaa2a4eb37ca377359f0a6ac807d72fc8015
abdoolly <abdallahgamal@Abdallahs-MacBook-Pro.local>
2020-02-27 19:23:23 +0200

    testing

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const { commandRunnerRequest } = require('../modules/commandRunner');
 
 router.get('/deploy', commandRunnerRequest);
 
+router.get('/test', (req, res) => res.send('hello world'));
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   return res.render('index', { title: 'Express' });
